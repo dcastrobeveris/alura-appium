358588c97efc269e8ea18e257f4489c37b851e26
Diego Barcellos <dcastrob@everis.com>
2021-07-15 17:04:04 -0300

    UI automator viewer

--- src/main/java/com/alura/appium/App.java
+++ src/main/java/com/alura/appium/App.java
@@ -12,15 +12,6 @@ import java.net.URL;
 public class App {
 
     public static void main( String[] args ) throws MalformedURLException {
-        File apk = new File("C:\\Users\\dcastrob\\intellij-workspace\\AluraAppium\\src\\main\\resources\\alura_esporte.apk");
-
-        DesiredCapabilities configuracoes = new DesiredCapabilities();
-        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
-        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-
-        URL urlConexao = new URL("http://127.0.0.1:4723/wd/hub");
-        AppiumDriver driver = new AppiumDriver<>(urlConexao,configuracoes);
 
 
     }
