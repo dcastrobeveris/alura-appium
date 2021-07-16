2ec5c985136835e1504bb1493bec18ef3aa26084
Diego Barcellos <dcastrob@everis.com>
2021-07-16 12:01:46 -0300

    Boas práticas com page object

--- src/test/java/com/alura/appium/FeatureCadastro.java
+++ src/test/java/com/alura/appium/FeatureCadastro.java
@@ -3,6 +3,7 @@ package com.alura.appium;
 import static org.junit.Assert.assertTrue;
 
 import com.alura.appium.PageObject.CadastroPageObject;
+import com.alura.appium.PageObject.LoginPageObject;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import org.junit.Assert;
@@ -18,10 +19,9 @@ public class FeatureCadastro {
     public void nao_consigo_cadastrar_usuario_com_senhas_que_nao_conferem() {
         AppiumDriver driver = AppiumDriverConfig.Instance().driver;
 
-        MobileElement botaoCadastro = (MobileElement)driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
-        botaoCadastro.click();
-
-        CadastroPageObject telaCadastro = new CadastroPageObject(driver);
+        LoginPageObject telaLogin = new LoginPageObject(driver);
+        telaLogin.BuscarElementos();
+        CadastroPageObject telaCadastro = telaLogin.IrParaTelaDeCadastro();
         telaCadastro.BuscarElementos();
         telaCadastro.Cadastrar("Diego", "123", "456");
         Assert.assertEquals("Senhas não conferem", telaCadastro.MensagemErro());
@@ -32,13 +32,11 @@ public class FeatureCadastro {
     public void posso_cadastrar_usuario_com_senhas_que_conferem() throws NoSuchElementException {
         AppiumDriver driver = AppiumDriverConfig.Instance().driver;
 
-        MobileElement botaoCadastro = (MobileElement)driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
-        botaoCadastro.click();
-
-        CadastroPageObject telaCadastro = new CadastroPageObject(driver);
+        LoginPageObject telaLogin = new LoginPageObject(driver);
+        telaLogin.BuscarElementos();
+        CadastroPageObject telaCadastro = telaLogin.IrParaTelaDeCadastro();
         telaCadastro.BuscarElementos();
-        telaCadastro.Cadastrar("Diego", "123", "123");
-
-        MobileElement botaoLogar = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/login_botao_logar");
+        telaLogin = telaCadastro.Cadastrar("Diego", "123", "123");
+        telaLogin.BuscarElementos();
     }
 }
