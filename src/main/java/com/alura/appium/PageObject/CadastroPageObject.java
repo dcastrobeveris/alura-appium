08c4d92d3cf0a63b0d3c0ed04bfba56e0bb743c7
Diego Barcellos <dcastrob@everis.com>
2021-07-16 13:46:55 -0300

    Esperando por elementos

--- src/main/java/com/alura/appium/PageObject/CadastroPageObject.java
+++ src/main/java/com/alura/appium/PageObject/CadastroPageObject.java
@@ -2,6 +2,9 @@ package com.alura.appium.PageObject;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CadastroPageObject {
 
@@ -36,7 +39,11 @@ public class CadastroPageObject {
     }
 
     public String MensagemErro(){
-        mensagemErro = (MobileElement)driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
+        By erroId = By.id("br.com.alura.aluraesporte:id/erro_cadastro");
+        WebDriverWait espera = new WebDriverWait(driver, 10);
+        espera.until(ExpectedConditions.presenceOfElementLocated(erroId));
+
+        mensagemErro = (MobileElement)driver.findElement(erroId);
         return mensagemErro.getText();
     }
 }
